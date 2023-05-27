664f494ff2069d32912776c57843a4b91485d7ff
Ngp55 <asdfrajkumar1122@gmail.com>
2023-05-28 00:38:30 +0530

    1st saved version with css controller index

---
+++ controllers/employee_controller.js
@@ -0,0 +1,7 @@
+module.exports.employee = function(req, res){
+    
+    return res.render('home',{
+        title: "EmployeePage",
+        body:"Welcome to Employee Page"
+    });
+} 
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+
+const app = express();
+const port = 9000;
+
+
+
+const expressLayouts = require('express-ejs-layouts');
+app.use(express.static('./assets'));
+// it is important to call expresslayouts before routes
+app.use(expressLayouts);
+// extracting style and scripts from sub pages in to the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+//Using express router
+app.use('/', require('./routes/indexR'));
+
+
+
+
+
+
+
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.listen(port , function(error){
+    if(error){
+        console.log(`Error in running the server: ${err}`);
+    }
+    console.log(`Server is running on port: ${port}`);
+})
