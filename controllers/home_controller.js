d5f5c5223d0861ef930456dacc91462f270e2a20
Ngp55 <asdfrajkumar1122@gmail.com>
2023-09-24 11:03:43 +0530

    Homepage url are setting to be rendered on singlepage

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -6,6 +6,7 @@ module.exports.home = async function(req, res){
     //  console.log(req.cookies);
     //  res.cookie('user_id', 24);
     //let user = User.findById(req.user.id);
+      
 
     let articles = await Articles.find({},'txtinput datetimeinput textarea catnames');
     //console.log(articles[0]);
@@ -18,11 +19,32 @@ module.exports.home = async function(req, res){
 };
 
 module.exports.singlepage = async function(req,res){
-    console.log(req.header);
+    console.log(req.params.id);
+    //const ArticleId = req.param.id;
 
-    let articles = await Articles.find({},'txtinput datetimeinput textarea catnames');
-    return res.render('single_Page',{
-        title:"SinglePage ||ThinkitToday",
-        articles:articles
-    });
+    // let articles = await Articles.find({},'txtinput datetimeinput textarea catnames');
+    // return res.render('single_Page',{
+    //     title:"SinglePage ||ThinkitToday",
+    //     articles:articles
+    // });
+    const articleId = req.params.id;
+
+    try {
+        // Use Mongoose's findById method to fetch the article by its ID
+        const article = await Articles.findById(articleId, 'txtinput datetimeinput textarea catnames');
+
+        if (!article) {
+            // Handle the case where the article is not found
+            return res.status(404).send('Article not found');
+        }
+
+        return res.render('single_Page', {
+            title: "SinglePage || ThinkitToday",
+            article: article // Pass the retrieved article to your template
+        });
+    } catch (err) {
+        // Handle any errors that occur during the database query
+        console.error(err);
+        return res.status(500).send('Internal Server Error');
+    }
 };
